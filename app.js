129f9aa1a2a12bc8da35f3b90b61a16ec228b2d3
emilsejr-design <emilerop@gmail.com>
2026-04-08 09:33:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -25,4 +25,34 @@ console.log("Efter ændring:", points);
 let mood = "træt";
 console.log("før:", mood);
 mood = "klar til javascript";
-console.log("efter:", mood)
+console.log("efter:", mood);
+
+const heading = document.querySelector("h1");
+console.log(heading);
+
+// Find h1 elementet
+const heading = document.querySelector("h1");
+console.log("Jeg fandt h1:", heading);
+
+// Find count span
+const countDisplay = document.querySelector("#counter");
+console.log("Jeg fandt count:", countDisplay);
+
+// Find knapperne
+const clickButton = document.querySelector("#click-button");
+const resetButton = document.querySelector("#reset-button");
+console.log("Jeg fandt knapperne:", clickButton, resetButton);
+
+// Ændr overskriften
+heading.textContent = "Wow, jeg kan ændre tekst! ";
+
+// Ændr count tallet
+countDisplay.textContent = "42";
+
+// Ændr tekstfarve
+heading.style.color = "yellow";
+
+// Ændr baggrund på count
+countDisplay.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+countDisplay.style.padding = "10px";
+countDisplay.style.borderRadius = "10px";
