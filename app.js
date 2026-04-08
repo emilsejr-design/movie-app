4f158c3529f61d99ffbba67bdcdfc237d83dbb40
emilsejr-design <emilerop@gmail.com>
2026-04-08 09:28:35 +0200

    commit

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-document.body.innerHTML += "Hello JavaScript!";
-// alert("Hello Dat.js");
-console.log("Hello Multimedia Designers");
-console.log("Hi Students!");
-console.log("This is your first JavaScript App 🎉");
+"use strict";
+
+const name = "Emil";
+const age = "25";
+const isStudent = true;
+
+console.log("Hello world");
+const message = "Javascript er sejt";
+console.log(message);
+
+const favoritMovie = "Into the wild" ;
+const year = "2007";
+const rating = "8";
+
+console.log ("min favorit film er:", favoritMovie);
+console.log ("den udkom i:", year);
+console.log ("rating:", rating);
+
+let points = 0;
+console.log("Start:", points);
+
+points = 1;
+console.log("Efter ændring:", points);
+
+let mood = "træt";
+console.log("før:", mood);
+mood = "klar til javascript";
+console.log("efter:", mood)
